d8f5f073a91add26ee2b93683b0055f1bc942324
nrobeR <mr.nrober@gmail.com>
2014-09-22 20:49:28 -0700

    (inc) trying to connect server to remote DB server

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 'use strict'
 
 var express = require('express');
+var mysql = require('mysql');
 
 var app = express();
 
 var server = require('http').createServer(app);
 
 var config = {};
+config.dbConnect = {
+  host: 'us-cdbr-azure-west-a.cloudapp.net',
+  user: 'b017f8a5a6d3e8',
+  password: '46c0073d',
+  database: 'ClickagoosDB'
+};
 config.port = process.env.PORT || 3000;
+
+var connection = mysql.createConnection(config.dbConnect);
+
+connection.connect();
+
+var helper = {};
+
+helper.counter = function(req,res){
+  connection.query('SELECT * FROM users', function(err,results){
+    if(err) res.send('err');
+    res.send(results);
+  })
+};
 
 server.listen(config.port,function(){
   console.log('Express server listenning to ', config.port);
@@ -16,5 +36,7 @@ server.listen(config.port,function(){
 app.get('/', function(req,res){
   res.send('hello world');
 });
+
+app.get('/counter', helper.counter);
 
 exports = module.exports = app;
