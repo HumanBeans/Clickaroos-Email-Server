5194e70f0f5ce09808fe39c9727e0f4578e9a21d
Joe Lowinske <lowi0008@gmail.com>
2014-10-01 15:07:13 -0700

    (feat) build initial ab_test email server logic

--- server/ab_test/ab_test.controller.js
+++ server/ab_test/ab_test.controller.js
@@ -1,5 +1,56 @@
 'use strict';
 
 var query = require('./ab_test.queries.js');
+var fs = require('fs');
 
-exports.serveImage = function() {};
+// var dummyABTestID = 1;
+// var dummyEmail = joe@gmail.com;
+// var dummyImages = ['1432.png', '6565.png', '4545.png'];
+
+exports.serveImage = function(req, res, endTime) {
+  var abTestID = req.params.ab_testID;
+  var userEmail = req.url.match(/\b[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b/g)[0];
+  var currentTime = new Date();
+
+
+
+
+  //check for file on server
+  path.open('' + '.png', 'r', function(err, fd) {
+    var image = '';
+    if(err) exports.getImage(image);
+
+
+
+  });
+};
+
+
+// retrieve stored image from Azure blob
+  exports.getImage = function(img) {
+    var file = fs.createWriteStream(img);  
+    http.get("http://clickaroos.blob.core.windows.net/img/client/" + img, function(response) {
+      console.log(response); 
+      response.pipe(file);
+      console.log('get img is done');
+    });
+  };
+
+  // display image when email is opened
+  exports.showImage = function(req, res) {
+    res.writeHead(200, {
+     'Content-Type' : 'image/png'
+    });
+   fs.createReadStream('./bg.jpg', { 'bufferSize': 4 * 1024 }).pipe(res);
+  };
+
+  // delete image from server when no longer required
+  exports.deleteImage = function(img) {
+    fs.unlink(img, function(err) {
+      if (err) throw err;
+      console.log('successfully deleted');
+    });
+  };
+
+
+};
--- server/routes.js
+++ server/routes.js
@@ -10,35 +10,19 @@
 var express = require('express');
 var fs = require('fs');
 var http = require('http');
-var ab = require('/ab_test/ab_test.controller.js');
+var ab = require('./ab_test/ab_test.controller.js');
 
 module.exports = function(app){
   // app.use('/clicks', imageRequestHandlers);
 
-  // retrieve stored image from Azure blob
-  app.get('/getImg', function(req, res){
-    var file = fs.createWriteStream("bg.jpg");
-    http.get("http://clickaroos.blob.core.windows.net/img/client/bg.jpg", function(response) {
-      console.log(response); 
-      response.pipe(file);
-      console.log('get img is done');
-    });
-  });
+  
+  app.get('/getImg', ab.getImage);
 
   // called to display image when email is opened
-  app.get('/showImg.png', function(req, res){
-    var img = fs.readFileSync('./bg.jpg');
-    res.writeHead(200, {'Content-Type': 'image/jpg' });
-    res.end(img, 'binary');
-  });
+  app.get('/showImg.png', ab.showImage);
 
-  // called to delete image from server when no longer required
-  app.get('/deleteImg', function(req, res) {
-    fs.unlink('./bg.jpg', function(err) {
-      if (err) throw err;
-      console.log('successfully deleted');
-    });
-  });
+  
+  app.get('/deleteImg', ab.deleteImage);
 
   app.get('/img/ab/:ab_testID/*', ab.serveImage);
 
