8b68e51bca6c7403de570e0e5f65187a56d7c3f7
Joe Lowinske <lowi0008@gmail.com>
2014-10-02 20:35:10 -0700

    (feat) work on ab.serveImages and helper functions

--- server/ab_test/ab_test.controller.js
+++ server/ab_test/ab_test.controller.js
@@ -1,35 +1,58 @@
 'use strict';
 
 var query = require('./ab_test.queries.js');
+var http = require('http');
 var fs = require('fs');
+var q = require('q');
+var bookshelf = require('../config/bookshelf_config.js');
+// var mem = require('../memcache.js');
 
+var Image = bookshelf.Model.extend({
+  tableName: 'ab_imgs'
+});
 
 // *** EMAIL SERVER FUNCTIONS ***
 exports.serveImage = function(req, res) {
   var abTestID = req.params.ab_testID;
   var userEmail = req.url.match(/\b[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9.-]+\b/g)[0];
-  var endTime; //bookshelf query for end_time
-  var campaignImages, randomImage, winner;
+  var endTime, campaignImages, servedImage;
 
-  if(abEndTimePassed(endTime)) {
-    //populate winner with bookshelf query for image with greatest clicks
-  } else {
-    // populate campaignImages with bookshelf query for all campaign images
-    randomImage = pickRandomImage(campaignImages);
+  // check memcache, if !in memcache, ping DB for campaign end_time, convert to Date() format
+  // FLAG: CHECK ALL VAR NAMES BELOW FOR COMPATIBILITY WITH MEMCACHE VAR NAMES
+  if(!abTestID in mem.memCache) {
+    exports.getAssociatedImages(req, res, abTestID);
+    return;
   }
 
-  if(winner) // serve winner
-  else if (randomImage) // serve randomImage 
-  else throw 'Error loading image'; 
+  campaignImages = [];
+  endTime = mem.cache[abTestID].endTime;
+  
+  for(var img in mem.cache[abTestID].imgs) {
+    campaignImages.push(img.fileName);
+  } 
 
-  });
+  if(abEndTimePassed(endTime)) {
+    // populate winner *** How will we know which is winner on memcache without pinging DB???
+    servedImage; // most clicks
+      
+      // if URL !in memcache -> get image from blob
+
+  } else {
+    // populate campaignImages with campaign URLs
+      // if URLs !in memcache -> get images from blob
+
+    servedImage = pickRandomImage(campaignImages);
+  }
+  //serve servedImage
 };
 
 
-// retrieve stored image from Azure blob
-  exports.getImage = function(img) {
-    var file = fs.createWriteStream(img);  
-    http.get("http://clickaroos.blob.core.windows.net/img/client/" + img, function(response) {
+
+  // retrieve stored image from Azure blob
+  exports.getImage = function(req) {
+    var imgUrl = typeof(req) === 'string' ? req : req.url.slice(8);
+    var file = fs.createWriteStream(imgUrl);
+    http.get("http://clickaroos.blob.core.windows.net/img/client/" + imgUrl, function(response) {
       console.log(response); 
       response.pipe(file);
       console.log('get img is done');
@@ -54,14 +77,65 @@ exports.serveImage = function(req, res) {
 
 
   // *** HELPER FUNCTIONS ***
-  var abEndTimePassed= function(endTime) {
+
+  // check if ab test trial period has elapsed
+  var abEndTimePassed = function(endTime) {
     var currentTime = new Date();
     if (currentTime > endTime) return true;
     else return false;
   };
 
+  // select random image to serve for ab test
   var pickRandomImage = function(imageArray) {
     var length = imageArray.length;
     var randomIndex = Math.floor(Math.random() * length);
     return imageArray[randomIndex];
   };
+
+  // find all asset urls associated with ab test and trigger downloadImages
+  exports.getAssociatedImages = function(res, req, abTestID) {
+    var imgArray = [];
+    
+    // query db for image models associated with ab test id
+    Image.collection().query().where({
+      ab_test_id: abTestID
+    }).select()
+
+    // populate imgArray with paths from Image models and write info to memcache
+    .then(function(modelArray) {
+      console.log('level 1');
+      modelArray.forEach(function(element, index, array) {
+        imgArray[index] = array[index]['asset_url'];
+      });
+
+      //TODO: write to memcache
+    })
+
+    // download images to server
+    .then(function() {
+      console.log('level 2');
+      downloadImages(res, req, imgArray);
+    })
+
+    // call serveImages again now that images have been downloaded to server
+    .then(function() {
+      console.log('level 3');
+      exports.serveImages(res, req);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  };
+
+  // download all images from Azure blob and trigger serveImages
+  var downloadImages = function(req, res, imgArray) {
+    imgArray.forEach(function(element, index, array) {
+      exports.getImage(array[index]);
+    });
+  };
+
+
+
+
+
+
