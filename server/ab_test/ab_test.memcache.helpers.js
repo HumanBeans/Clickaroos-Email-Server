a7fd1c45ec8ebae1806279af3a31aaad0fa21bb5
nrobeR <mr.nrober@gmail.com>
2014-10-08 22:53:35 -0700

    (feat) Send device data to database

--- server/ab_test/ab_test.memcache.helpers.js
+++ server/ab_test/ab_test.memcache.helpers.js
@@ -24,6 +24,7 @@ exports.updateImgsClicksAndViews = function( that, ABTestID ) {
 
 exports.updateCampaignsClicksAndViews = function( that, ABTestID ) {
   var campaignID, totalViews = 0, totalClicks = 0, ab_imgs = that[ ABTestID ].imgs;
+  var campaign_data = {};
   AbTest.where( {ab_test_id: ABTestID} )
         .fetch()
         .then(function( abRow ){ 
@@ -37,8 +38,22 @@ exports.updateCampaignsClicksAndViews = function( that, ABTestID ) {
                    .fetch()
                    .then(function( campaignRow ){
                       // campaignClicks = campaignRow.attributes.clicks + totalClicks;
+                      campaign_data.iphone = that[ABTestID].device['iPhone'];
+                      campaign_data.ipad = that[ABTestID].device['iPad'];
+                      campaign_data.android_phone = that[ABTestID].device['Android Phone'];
+                      campaign_data.android_pad = that[ABTestID].device['Android Tablet'];
+                      campaign_data.desktop = that[ABTestID].device['Desktop'];
+                      campaign_data.device_other = that[ABTestID].device['Other'];
+
+
+                      campaign_data.clicks = totalClicks;
+                      campaign_data.views = totalViews;
+
+                      console.log('campaign_data========', campaign_data);
+
                       Campaigns.where( { campaign_id: campaignID } )
-                               .save( { clicks: totalClicks, views: totalViews }, {method: 'update'} );
+                               .save( campaign_data, {method: 'update'} );
                    })
         }) 
 };
+
