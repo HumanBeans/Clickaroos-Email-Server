e4bd7f6cdc8535292e132cd3648e16231464f7f4
Dara Bun <dara209@gmail.com>
2014-10-08 20:49:40 -0700

    last log

--- server/routes.js
+++ server/routes.js
@@ -21,35 +21,59 @@ module.exports = function(app){
   // app.use('/clicks', imageRequestHandlers);
   
   app.get('/ua', function(req, res) {
-    var ua = req.headers['user-agent'];
-    console.log( parser.setUA(ua).getResult());
-    // res.json('ua: ', ua.toJSON()); 
-    var type = "type: " + parser.setUA(ua).getDevice().type; // tablet, mobile, console
-    var model = "## model: " + parser.setUA(ua).getDevice().model; // iPhone, iPad, GT-I905
-    var device = "## device: " + parser.setUA(ua).getOS().name; // Android, iOS, Mac OS, Windows
 
-    // if model === iPhone
-      // return iPhone
-    // if model === iPad
-      // return iPad
-
-    // if type === tablet
-      // if OS.name === Android
-        // return Android tablet
-
-    // if type === mobile
-      // if OS.name === Android
-        // return Android mobile
-
-    // if name === Mac OS || Windows || Ubuntu || Mint
+    // if name === Mac OS X|| Windows || Ubuntu || Mint
       // return Desktop
-
+    console.log( 'FINAL Device: ', checkDevice(res, res) );
+    
     console.log( type + model + device );
     res.json( type + model + device );
   });
 
   function checkDevice(req, res) {
+    var ua = req.headers['user-agent'];
+    console.log( parser.setUA(ua).getResult());
 
+    var type = "type: " + parser.setUA(ua).getDevice().type; // tablet, mobile, console
+    var model = "## model: " + parser.setUA(ua).getDevice().model; // iPhone, iPad, GT-I905
+    var device = "## device: " + parser.setUA(ua).getOS().name; // Android, iOS, Mac OS X, Windows
+
+    // if model === iPhone
+      // return iPhone
+    if( device === 'iPhone' ) {
+      return 'iPhone';
+    }
+    // if model === iPad
+      // return iPad
+    if( device === 'iPad' ) {
+      return 'iPad';
+    }
+
+    // if type === tablet
+    if ( type === 'tablet') {
+      // if OS.name === Android
+      if ( device === 'Android' ) {
+        // return Android tablet
+        return 'Android Tablet';
+      }
+    }
+
+    // if type === mobile
+    if ( type === 'mobile') {
+      // if OS.name === Android
+      if ( device === 'Android' ) {
+        // return Android mobile
+        return 'Android Phone';
+      }
+    }
+
+    if ( name.indexOf('Mac') !== -1 || 
+          name.indexOf('Windows') !== -1 ||
+          name.indexOf('Ubuntu') !== -1 ||
+          name.indexOf('Mint') !== -1 ) 
+    {
+        return 'Desktop';
+    }
   };
 
   app.get('/getImg/*', function(req) {
